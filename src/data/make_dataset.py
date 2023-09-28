cf375acd78c8338010d1ae2150cdc9f5ec67746f
dbstjdghks03 <snu15119@snu.ac.kr>
2023-09-29 00:10:45 +0900

    project structure

---
+++ src/data/make_dataset.py
@@ -0,0 +1,7 @@
+"""
+This directory holds scripts that handle data processing.
+
+It may include files like
+"make_dataset.py" (a script to download, filter, preprocess, and partition the raw data into training and test splits),
+"preprocessing.py" (a script containing functions for data cleaning and preparation for modeling).
+"""
