8900b4866ed9e82184a6a6e39c5766e48ff12b4b
dbstjdghks03 <snu15119@snu.ac.kr>
2023-10-01 01:12:10 +0900

    project structure

--- test.py
+++ test.py
@@ -1,1 +1,63 @@
-print('a')
+import torch
+import torch.nn as nn
+import torchvision.models as models
+from sklearn.decomposition import PCA
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.model_selection import train_test_split
+import numpy as np
+
+# Load the pre-trained ResNet-50 model
+resnet50 = models.resnet50(pretrained=True)
+modules = list(resnet50.children())[:-1]
+resnet50 = nn.Sequential(*modules)
+for param in resnet50.parameters():
+    param.requires_grad = False
+
+# Define your audio dataset loading and processing code here
+# You'll need to load and preprocess your audio data into a suitable format
+
+# Extract features using ResNet-50
+def extract_resnet50_features(audio_data):
+    features = []
+    for audio_sample in audio_data:
+        # Preprocess audio_sample if needed (e.g., convert to spectrogram)
+        # Convert audio_sample to a tensor
+        audio_tensor = torch.tensor(audio_sample)
+        # Forward pass through ResNet-50
+        with torch.no_grad():
+            features.append(resnet50(audio_tensor).squeeze().numpy())
+    return np.array(features)
+
+# Load and preprocess your audio dataset
+# audio_data, labels = load_and_preprocess_audio_data()
+
+# Extract features using ResNet-50
+features = extract_resnet50_features(audio_data)
+
+# Apply PCA for dimensionality reduction
+n_components = 30  # Define the number of principal components
+pca = PCA(n_components=n_components)
+reduced_features = pca.fit_transform(features)
+
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(reduced_features, labels, test_size=0.2, random_state=42)
+
+# Train an SVM classifier
+svm_classifier = SVC(kernel='rbf', C=1.0)
+svm_classifier.fit(X_train, y_train)
+
+# Make predictions on the test set
+y_pred = svm_classifier.predict(X_test)
+
+# Evaluate the classifier
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+
+# Print evaluation metrics
+print(f"Accuracy: {accuracy}")
+print(f"Precision: {precision}")
+print(f"Recall: {recall}")
+print(f"F1 Score: {f1}")
