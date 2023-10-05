70e9ad6e03caf7511ddbf4f027172c5568d6c529
dbstjdghks03 <snu15119@snu.ac.kr>
2023-10-06 04:10:04 +0900

    train_json

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -5,3 +5,32 @@ It may include files like
 "make_dataset.py" (a script to download, filter, preprocess, and partition the raw data into training and test splits),
 "preprocessing.py" (a script containing functions for data cleaning and preparation for modeling).
 """
+import torch
+from torch.utils.data import Dataset
+
+class AudioFeatureDataset(Dataset):
+    def __init__(self, stft_data, mfcc_data, sc_data, labels):
+        self.stft_data = stft_data
+        self.mfcc_data = mfcc_data
+        self.sc_data = sc_data
+        self.labels = labels
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        stft = self.stft_data[idx]
+        mfcc = self.mfcc_data[idx]
+        sc = self.sc_data[idx]
+        label = self.labels[idx]
+
+        return stft, mfcc, sc, label
+
+#데이터 로딩 및 DataLoader 생성:
+# 예를 들어, stft_data, mfcc_data, sc_data, labels를 준비한 후
+Data = AudioFeatureDataset()
+
+custom_dataset = AudioFeatureDataset(Data.stft, Data.mfcc, Data.sc, Data.label)
+
+# DataLoader 생성
+dataloader = torch.utils.data.DataLoader(custom_dataset, batch_size=32, shuffle=True)
--- src/datasets/first_dataset.py
+++ src/datasets/first_dataset.py
@@ -1,6 +1,3 @@
 #데이터 로딩 및 DataLoader 생성:
 # 예를 들어, stft_data, mfcc_data, sc_data, labels를 준비한 후
-custom_dataset = AudioFeatureDataset(stft_data, mfcc_data, sc_data, labels)
-
-# DataLoader 생성
-dataloader = torch.utils.data.DataLoader(custom_dataset, batch_size=32, shuffle=True)
+tils.data.DataLoader(custom_dataset, batch_size=32, shuffle=True)
