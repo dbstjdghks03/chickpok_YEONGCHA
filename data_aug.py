a580e1bb2af3687abf291675fb383fa1b9842b49
Hijetsurgery <131605667+Hijetsurgery@users.noreply.github.com>
2023-10-15 17:01:17 +0900

    Update data_aug.py

--- data_aug.py
+++ data_aug.py
@@ -1,50 +1,46 @@
 from preprocessing import PreProcess
-import torch
 import numpy as np
-import random
+
 import librosa
 import matplotlib.pyplot as plt
 
 
 
-class data_aug:
-    def __init__(self, data) -> None:
-        self.data = data
+class noise(object):
+    def __init__(self, noise_factor) -> None:
+        self.noise_factor = noise_factor
 
-    '''def comparison(func, y):
-        aug_y = func(y, 0.1)
-
-        mfcc = librosa.feature.mfcc(y=y, sr=25600, n_mfcc=100, n_fft=640, hop_length=256)
-        aug_mfcc = librosa.feature.mfcc(y=aug_y, sr=25600, n_mfcc=100, n_fft=640, hop_length=256)
-
-        #librosa.display.specshow(mfcc, sr=25600, x_axis='ms')
-        librosa.display.specshow(aug_mfcc, sr=25600, x_axis='ms')'''
-
-
-    def noising(self, noise_factor):
+    def __call__(self, data):
         """
         원본 데이터에 노이즈를 추가합니다.
         noise factor를 통해 조절합니다.
         """
-        noise = np.random.randn(len(self.data))
-        augmented_data = self.data + noise_factor * noise
+        noise = np.random.randn(len(data))
+        augmented_data = data + self.noise_factor * noise
         # Cast back to same data type
-        augmented_data = augmented_data.astype(type(self.data[0]))
+        augmented_data = augmented_data.astype(type(data[0]))
         return augmented_data
 
-    def shifting(self, sampling_rate, shift_max, shift_direction):
+
+class shifting(object):
+    def __init__(self, sampling_rate, shift_max, shift_direction) -> None:
+        self.sampling_rate = sampling_rate
+        self.shift_max = shift_max
+        self.shift_direction = shift_direction
+
+    def __call__(self, data):
         """
         원본 데이터를 좌우로 이동시킵니다.
         shift_max를 통해 최대 얼마까지 이동시킬지 조절합니다.
         """
-        shift = np.random.randint(sampling_rate * shift_max+1)
-        if shift_direction == 'right':
+        shift = np.random.randint(self.sampling_rate * self.shift_max+1)
+        if self.shift_direction == 'right':
             shift = -shift
-        elif shift_direction == 'both':
+        elif self.shift_direction == 'both':
             direction = np.random.randint(0, 2)
             if direction == 1:
                 shift = -shift
-        augmented_data = np.roll(self.data, shift)
+        augmented_data = np.roll(data, shift)
         # Set to silence for heading/ tailing
         if shift > 0:
             augmented_data[:shift] = 0
@@ -52,9 +48,15 @@ class data_aug:
             augmented_data[shift:] = 0
         return augmented_data
 
-    def change_pitch(self, sampling_rate, pitch_factor):
+
+class change_pitch(object):
+    def __init__(self, sampling_rate, pitch_factor) -> None:
+        self.sampling_rate = sampling_rate
+        self.pitch_factor = pitch_factor
+
+    def __call__(self, data):
         """
         원본 데이터의 피치를 조절합니다.
         """
-        return librosa.effects.pitch_shift(self.data, sampling_rate, pitch_factor)
+        return librosa.effects.pitch_shift(data, self.sampling_rate, self.pitch_factor)
 
