154dc543fa16483a612b89610b94466ca1ddc6c2
pswcsj <pswxsj@naver.com>
2023-10-20 05:43:19 +0900

    delete torch

--- src/models/loss.py
+++ src/models/loss.py
@@ -5,6 +5,8 @@ from torch import nn
 def loss(output, horn, position, alpha=1e-3, beta=1):
     mse_loss = nn.MSELoss()
     mask = (horn == -1).float()
+    print(torch.mean(torch.clamp(1 - output[:, 0] * horn, min=0)), mse_loss(output[:, 1]*mask, position*mask))
+
     train_loss = alpha * torch.mean(torch.clamp(1 - output[:, 0] * horn, min=0)) + beta * mse_loss(output[:, 1]*mask, position*mask)
 
     return train_loss
