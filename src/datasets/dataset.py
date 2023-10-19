add mse masking add prediction and test dataset fixed svm system

src/datasets/dataset.py
@@ -22,7 +22,7 @@ train_to_idx = {
     '차세대전동차': 1
 }
 class_to_idx = {
-    'Yes': 0,
+    'Yes': -1,
     'No': 1
 }
 
@@ -138,7 +138,7 @@ class YoungDataSet(Dataset):
                     train = train_to_idx[data['Train']]
                     horn = class_to_idx[data['Horn']]
 
-                    if horn == 0:
+                    if horn == -1:
                         position = int(data['Position'])
                         if train == 0:
                             cluster = 'CL_HY'
@@ -150,12 +150,11 @@ class YoungDataSet(Dataset):
                             cluster = 'CL_HN'
                         else:
                             cluster = 'CL_NN'
-
-                    self.data_list.append([s206_path, batcam_path, train, horn, position, cluster, data])
+                    s206_audio = np.load(self.root+s206_path)
+                    self.data_list.append([s206_audio, batcam_path, train, horn, position, cluster, data])
 
     def __getitem__(self, idx):
-        s206_path, batcam_path, _, horn, position, _, _ = self.data_list[idx]
-        s206_audio = np.load(self.root+s206_path)
+        s206_audio, batcam_path, _, horn, position, _, _ = self.data_list[idx]
         # s206_audio = TdmsFile(self.root + s206_path)
         # batcam_audio, batcam_beam = tdms_preprocess(self.root + batcam_path)
         s206 = PreProcess(s206_audio)

src/models/loss.py
@@ -4,6 +4,7 @@ from torch import nn
 
 def loss(output, horn, position):
     mse_loss = nn.MSELoss()
-    train_loss = torch.mean(torch.clamp(1 - output[:, 0] * horn, min=0)) + mse_loss(output[:, 1], position)
+    mask = (horn == -1).float()
+    train_loss = torch.mean(torch.clamp(1 - output[:, 0] * horn, min=0)) + mse_loss(output[:, 1]*mask, position*mask)
 
     return train_loss

train.py
@@ -6,6 +6,8 @@ from torch.utils.data import DataLoader
 from src.models.PCAmodel import PCAModel
 from sklearn.model_selection import StratifiedKFold
 from src.models.loss import loss
+import matplotlib.pyplot as plt
+import torch.nn as nn
 
 parser = argparse.ArgumentParser()
 
@@ -24,20 +26,16 @@ n_components = 10
 print(root)
 dataset = YoungDataSet(root=root, is_npy=True)
 
-# train_len = int(0.8 * len(dataset))
-# val_len = len(dataset) - train_len
-# print(train_len, val_len)
-
 data_list = dataset.data_list
 
 skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
-print([item[5] for item in data_list])
-
+MSELoss = nn.MSELoss()
 if __name__ == '__main__':
     model = PCAModel(n_components).to(device)
     # optimizer로는 Adam 사용
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 
+    valid_best = 99999999
     for fold, (train_indices, test_indices) in enumerate(skf.split(data_list, [item[5] for item in data_list])):
         train_set = torch.utils.data.Subset(dataset, train_indices)
         test_set = torch.utils.data.Subset(dataset, test_indices)
@@ -47,50 +45,49 @@ if __name__ == '__main__':
 
         train_losses = []
         test_losses = []
+
         for epoch in range(epochs):
             model.train()
-            print(f"{epoch}th epoch starting.")
-            running_test_loss = 0
+            print(f"font {fold}: {epoch}th epoch starting.")
+            epoch_test_loss = 0
+            epoch_train_loss = 0
             for i, (stft, mfcc, sc, horn, position) in enumerate(train_loader):
-                stft, mfcc, sc, horn, position = stft.to(device).float(), mfcc.to(device).float(), sc.to(device).float(), horn.to(device), position.to(device).float()
+                stft, mfcc, sc, horn, position = stft.to(device).float(), mfcc.to(device).float(), sc.to(
+                    device).float(), horn.to(device), position.to(device).float()
                 optimizer.zero_grad()
                 output = model(mfcc, sc)
                 train_loss = loss(output, horn, position)
-                print(train_loss)
-
+                epoch_train_loss += train_loss.item()
                 train_loss.backward()
                 optimizer.step()
 
-    #     model.eval()
-    #     running_train_loss = 0.0
-    #     running_test_loss = 0.0
-    #     for i, (images, labels) in enumerate(train_dataloader, 0):
-    #         images, labels = images.to(device), labels.to(device)
-    #         running_train_loss += loss_function(model(images), labels).item() / images.shape[0]
-    #     for i, (images, labels) in enumerate(test_dataloader, 0):
-    #         images, labels = images.to(device), labels.to(device)
-    #         running_test_loss += loss_function(model(images), labels).item() / images.shape[0]
-    #     train_losses.append(running_train_loss)
-    #     test_losses.append(running_test_loss)
-    #
-    # test_loss, correct, total = 0, 0, 0
-    # for i, (images, labels) in enumerate(test_dataloader):
-    #     images, labels = images.to(device), labels.to(device)
-    #
-    #     output = model(images)
-    #     test_loss += loss_function(output, labels).item()
-    #
-    #     pred = output.max(1, keepdim=True)[1]
-    #     correct += pred.eq(labels.view_as(pred)).sum().item()
-    #
-    #     total += labels.size(0)
-    #
-    # print('[Test set] Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
-    #     test_loss / total, correct, total,
-    #     100. * correct / total))
-    # plt.plot(test_losses, label="test_loss")
-    # plt.plot(train_losses, label="train_loss")
-    # plt.legend()
-    # plt.show()
-    #
-    # torch.save(model.state_dict(), 'model.pt')
+            model.eval()
+            position_mse = 0
+            for i, (stft, mfcc, sc, horn, position) in enumerate(test_loader):
+                stft, mfcc, sc, horn, position = stft.to(device).float(), mfcc.to(device).float(), sc.to(
+                    device).float(), horn.to(device), position.to(device).float()
+                output = model(mfcc, sc)
+                epoch_test_loss += loss(output, horn, position).item()
+
+                predictions = torch.tensor([1 if 1 - out[0] > 0 else -1 for out in output])
+                correct_predictions = (predictions == horn).float().sum()
+                position_mse += MSELoss(output[:, 1], position)
+
+            accuracy = correct_predictions / len(test_loader)
+
+            print(f'train_loss: {epoch_train_loss}')
+            print('[Test set] Average loss: {:.4f}, Horn Accuracy: {}/{} ({:.2f}%), Position MSE{}\n'.format(
+                epoch_test_loss / len(test_loader), correct_predictions, len(test_loader),
+                accuracy, position_mse))
+
+            train_losses.append(epoch_train_loss)
+            test_losses.append(epoch_test_loss)
+            if valid_best > epoch_test_loss:
+                valid_best = epoch_test_loss
+                torch.save(model.state_dict(), 'valid_best_model.pt')
+
+    plt.plot(test_losses, label="test_loss")
+    plt.plot(train_losses, label="train_loss")
+    plt.legend()
+    plt.show()
+