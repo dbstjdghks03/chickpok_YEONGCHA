bf4bb47a56d1b6bd3743bb57ecc826115e9c6b68
pswcsj <pswxsj@naver.com>
2023-10-20 06:54:50 +0900

    pirnt debug

--- src/models/PCAmodel.py
+++ src/models/PCAmodel.py
@@ -52,9 +52,11 @@ class PCA(nn.Module):
         self.n_components = n_components
 
     def forward(self, x):
+        print(x.shape)
         mean = torch.mean(x, dim=0)
         std = torch.std(x, dim=0) + 1e-5
         x = (x - mean) / std
+        print(mean.shape, std.shape)
         print(torch.any(torch.isnan(x)))
         try:
             U, S, Vt = torch.linalg.svd(x)
