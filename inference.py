80fe53e1df790519bca9cbe1997d6b3a9ca65b41
pswcsj <pswxsj@naver.com>
2023-10-23 02:49:16 +0900

    add to_cxs

--- inference.py
+++ inference.py
@@ -9,6 +9,7 @@ from src.models.PCAmodel import PCAModel
 import pandas as pd
 from torch.utils.data import DataLoader
 import os
+import torch.nn as nn
 parser = argparse.ArgumentParser()
 
 # 3. parser.add_argument로 받아들일 인수를 추가해나간다.
